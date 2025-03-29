c60dc1f38b232dc2f00cfbd0033ffcdcaa318db6
HubertFer <humbertofernandezsosa@gmail.com>
2025-03-29 06:00:41 -0300

    DTO creation for data management

---
+++ src/main/java/br/com/udemy/tasks/controller/dto/TaskDTO.java
@@ -0,0 +1,15 @@
+package br.com.udemy.tasks.controller.dto;
+
+import br.com.udemy.tasks.model.TaskState;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class TaskDTO {
+
+    private String title;
+    private String description;
+    private int priority;
+    private TaskState state;
+}
